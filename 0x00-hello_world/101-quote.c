3ba57cf9787bfbc1301d6c4f2a25791237c5810d
LionHardy <leonardndobe@gmail.com>
2023-02-17 20:08:26 -0800

    7. Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <std.h>
+#include <unistd.h>
+/**
+ * main - standard error printing program
+ * Return: 1 (Success)
+ */
+int main(void)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
