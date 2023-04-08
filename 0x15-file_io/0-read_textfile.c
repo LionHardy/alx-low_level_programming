ce167495a1773e069ad7d8797f39d31654e33ed5
LionHardy <leonardndobe@gmail.com>
2023-04-08 15:22:02 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+/**
+ * read_textfile - read a text file and prints it to posix stdout
+ * @filename: name of the file containing letters
+ * @letters: number of letters
+ *
+ * Return: 0 if filename is NULL
+ * 0 if file cannot be read or opened
+ * 0 if write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	FILE *filename;
+
+	filename = fopen("filename.txt", O_RDONLY);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fprintf(filename, "%s", "letters");
+
+	fclose(filename);
+
+	return (0);
+}
